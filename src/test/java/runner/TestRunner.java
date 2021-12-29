263137b0ee3babc8992b5a67fd80bfeff8ad21b8
Pratip Banerjee <pratipbanerjee1991@gmail.com>
2021-12-29 23:23:10 +0530

    Update 29th Dec

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = "src\\test\\Resources\\Features",
+        features = "src\\test\\resources\\Features",
         glue = "stepdefinations"
 )
 
